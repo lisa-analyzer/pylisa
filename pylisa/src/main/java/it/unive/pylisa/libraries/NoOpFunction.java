0ade17547b4eaf96e105a042741eb47adca21da0
lucaneg <luca.negrinii@gmail.com>
2022-03-29 15:54:13 +0200

    Adding NoOpFunction for functions that behave as a noop

---
+++ pylisa/src/main/java/it/unive/pylisa/libraries/NoOpFunction.java
@@ -0,0 +1,46 @@
+package it.unive.pylisa.libraries;
+
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.analysis.heap.HeapDomain;
+import it.unive.lisa.analysis.lattices.ExpressionSet;
+import it.unive.lisa.analysis.value.TypeDomain;
+import it.unive.lisa.analysis.value.ValueDomain;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.NaryExpression;
+import it.unive.lisa.program.cfg.statement.NoOp;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.symbolic.SymbolicExpression;
+
+public abstract class NoOpFunction extends NaryExpression implements PluggableStatement {
+
+	protected Statement st;
+
+	protected NoOpFunction(CFG cfg, CodeLocation location, String constructName,
+			Expression... parameters) {
+		super(cfg, location, constructName, parameters);
+	}
+
+	@Override
+	final public void setOriginatingStatement(Statement st) {
+		this.st = st;
+	}
+
+	@Override
+	public <A extends AbstractState<A, H, V, T>,
+			H extends HeapDomain<H>,
+			V extends ValueDomain<V>,
+			T extends TypeDomain<T>> AnalysisState<A, H, V, T> expressionSemantics(
+					InterproceduralAnalysis<A, H, V, T> interprocedural,
+					AnalysisState<A, H, V, T> state,
+					ExpressionSet<SymbolicExpression>[] params,
+					StatementStore<A, H, V, T> expressions) throws SemanticException {
+		return new NoOp(getCFG(), getLocation()).semantics(state, interprocedural, expressions);
+	}
+}
--- pylisa/src/main/java/it/unive/pylisa/libraries/standardLibrary/Print.java
+++ pylisa/src/main/java/it/unive/pylisa/libraries/standardLibrary/Print.java
@@ -3,9 +3,9 @@ package it.unive.pylisa.libraries.standardLibrary;
 import it.unive.lisa.program.cfg.CFG;
 import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.statement.Expression;
-import it.unive.pylisa.libraries.UninterestingDataframeFunction;
+import it.unive.pylisa.libraries.NoOpFunction;
 
-public class Print extends UninterestingDataframeFunction {
+public class Print extends NoOpFunction {
 	public Print(CFG cfg, CodeLocation location, String constructName, Expression... parameter) {
 		super(cfg, location, constructName, parameter);
 	}
