3c3734558e5f0a47826af557a6b3ab244ede44bd
Luca Negrini <luca.negrinii@gmail.com>
2023-05-18 18:18:24 +0200

    Restructuring notebook tests

--- pylisa/src/test/java/it/unive/pylisa/notebooks/PyTransformationsTest.java
+++ pylisa/src/test/java/it/unive/pylisa/notebooks/PyTransformationsTest.java
@@ -8,22 +8,27 @@ public class PyTransformationsTest extends NotebookTest {
 
 	@Test
 	public void testCovid19() throws IOException, AnalysisException {
-		performAndCheck("dataframes-tests/covid-19.py");
+		performAndCheck("dataframes-tests/covid-19.ipynb");
+	}
+
+	@Test
+	public void testCreditFraud() throws IOException, AnalysisException {
+		perform("dataframes-tests/credit-fraud.ipynb");
+	}
+
+	@Test
+	public void testDataExploration() throws IOException, AnalysisException {
+		perform("dataframes-tests/data-exploration.ipynb");
+	}
+
+	@Test
+	public void testTitanic() throws IOException, AnalysisException {
+		perform("dataframes-tests/titanic.ipynb");
 	}
 
 	@Test
 	public void testCovid19Updated() throws IOException, AnalysisException {
 		performAndCheck("dataframes-tests/covid-19-updated.py");
-	}
-
-	@Test
-	public void testCreditFraud() throws IOException, AnalysisException {
-		perform("dataframes-tests/credit-fraud.py");
-	}
-
-	@Test
-	public void testDataExploration() throws IOException, AnalysisException {
-		perform("dataframes-tests/data-exploration.py");
 	}
 
 	@Test
@@ -34,10 +39,5 @@ public class PyTransformationsTest extends NotebookTest {
 	@Test
 	public void testGuideSmall() throws IOException, AnalysisException {
 		performAndCheck("dataframes-tests/guide-small.py");
-	}
-
-	@Test
-	public void testTitanic() throws IOException, AnalysisException {
-		perform("dataframes-tests/titanic.py");
 	}
 }
