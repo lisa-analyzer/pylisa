e8a02b136d52670d9e4994b11b8fcc1649399f2d
VincenzoArceri <vincenzoarceri.92@gmail.com>
2026-09-09 13:59:59 +0200

    Basic abstract semantics for multiplication (int and float)

--- pylisa/py-testcases/basic/basic.py
+++ pylisa/py-testcases/basic/basic.py
@@ -2,8 +2,10 @@ x = 1
 y = 2
 z = x + y
 w = x - y
+v = x * y
 
 a = 1.5
 b = 2.5
 c = a + b
 d = a - b
+e = a * b
--- pylisa/src/main/java/it/unive/pylisa/cfg/expression/PyMultiplication.java
+++ pylisa/src/main/java/it/unive/pylisa/cfg/expression/PyMultiplication.java
@@ -6,10 +6,15 @@ import it.unive.lisa.analysis.Analysis;
 import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.analysis.symbols.SymbolAliasing;
 import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.interprocedural.callgraph.CallResolutionException;
 import it.unive.lisa.program.cfg.CFG;
 import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.call.Call.CallType;
+import it.unive.lisa.program.cfg.statement.call.UnresolvedCall;
+import it.unive.lisa.program.cfg.statement.evaluation.LeftToRightEvaluation;
 import it.unive.lisa.program.cfg.statement.numeric.Multiplication;
 import it.unive.lisa.symbolic.SymbolicExpression;
 import it.unive.lisa.symbolic.value.BinaryExpression;
@@ -17,6 +22,7 @@ import it.unive.lisa.type.Type;
 import it.unive.pylisa.libraries.LibrarySpecificationProvider;
 import it.unive.pylisa.libraries.PyLibraryUnitType;
 import it.unive.pylisa.symbolic.operators.StringMult;
+import java.util.Collections;
 import java.util.Set;
 
 public class PyMultiplication extends Multiplication {
@@ -59,6 +65,51 @@ public class PyMultiplication extends Multiplication {
 							getLocation()),
 					this);
 		}
-		return super.fwdBinarySemantics(interprocedural, state, left, right, expressions);
+
+		SymbolAliasing aliasing = state.getExecutionInfo(SymbolAliasing.INFO_KEY, SymbolAliasing.class);
+
+		AnalysisState<A> result = state.bottom();
+		for (Type tl : rtsl) {
+			for (Type tr : rtsr) {
+				if (tr.canBeAssignedTo(tl)) {
+					// int * int (and subtypes thereof): call int.__mul__,
+					// falling back to int.__rmul__ if it does not resolve
+					UnresolvedCall mul = new UnresolvedCall(
+							getCFG(),
+							getLocation(),
+							CallType.STATIC,
+							null,
+							"__mul__",
+							LeftToRightEvaluation.INSTANCE,
+							getLeft(),
+							getRight());
+					boolean mulResolves;
+					try {
+						interprocedural.resolve(mul,
+								new Set[] { Collections.singleton(tl), Collections.singleton(tr) }, aliasing);
+						mulResolves = true;
+					} catch (CallResolutionException e) {
+						mulResolves = false;
+					}
+
+					if (mulResolves)
+						result = result.lub(mul.forwardSemantics(state, interprocedural, expressions));
+					else {
+						UnresolvedCall rmul = new UnresolvedCall(
+								getCFG(),
+								getLocation(),
+								CallType.STATIC,
+								null,
+								"__rmul__",
+								LeftToRightEvaluation.INSTANCE,
+								getRight(),
+								getLeft());
+						result = result.lub(rmul.forwardSemantics(state, interprocedural, expressions));
+					}
+				}
+			}
+		}
+
+		return result;
 	}
 }
