30ae78f0fe36c231d545b9feead47d996b219b5f
VincenzoArceri <vincenzoarceri.92@gmail.com>
2026-09-09 16:14:00 +0200

    Refactoring packages

---
+++ pylisa/src/main/java/it/unive/pylisa/libraries/ints/IntAdd.java
@@ -0,0 +1,70 @@
+package it.unive.pylisa.libraries.ints;
+
+import it.unive.lisa.analysis.AbstractDomain;
+import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.BinaryExpression;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.operator.binary.NumericNonOverflowingAdd;
+
+/**
+ * Native implementation of {@code int.__add__(self, other)}.
+ */
+public class IntAdd extends BinaryExpression implements PluggableStatement {
+
+	protected Statement st;
+
+	protected IntAdd(
+			CFG cfg,
+			CodeLocation location,
+			String constructName,
+			Expression self,
+			Expression other) {
+		super(cfg, location, constructName, self, other);
+	}
+
+	public static IntAdd build(
+			CFG cfg,
+			CodeLocation location,
+			Expression[] exprs) {
+		return new IntAdd(cfg, location, "__add__", exprs[0], exprs[1]);
+	}
+
+	@Override
+	final public void setOriginatingStatement(
+			Statement st) {
+		this.st = st;
+	}
+
+	@Override
+	protected int compareSameClassAndParams(
+			Statement o) {
+		return 0;
+	}
+
+	@Override
+	public <A extends AbstractLattice<A>, D extends AbstractDomain<A>> AnalysisState<A> fwdBinarySemantics(
+			InterproceduralAnalysis<A, D> interprocedural,
+			AnalysisState<A> state,
+			SymbolicExpression left,
+			SymbolicExpression right,
+			StatementStore<A> expressions)
+			throws SemanticException {
+		return interprocedural.getAnalysis().smallStepSemantics(state,
+				new it.unive.lisa.symbolic.value.BinaryExpression(
+						getStaticType(),
+						left,
+						right,
+						NumericNonOverflowingAdd.INSTANCE,
+						getLocation()),
+				st);
+	}
+}
---
+++ pylisa/src/main/java/it/unive/pylisa/libraries/ints/IntInvert.java
@@ -0,0 +1,67 @@
+package it.unive.pylisa.libraries.ints;
+
+import it.unive.lisa.analysis.AbstractDomain;
+import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.AnalysisState;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.StatementStore;
+import it.unive.lisa.interprocedural.InterproceduralAnalysis;
+import it.unive.lisa.program.cfg.CFG;
+import it.unive.lisa.program.cfg.CodeLocation;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.lisa.program.cfg.statement.PluggableStatement;
+import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.program.cfg.statement.UnaryExpression;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.operator.unary.BitwiseNegation;
+
+/**
+ * Native implementation of {@code int.__invert__(self)}.
+ */
+public class IntInvert extends UnaryExpression implements PluggableStatement {
+
+	protected Statement st;
+
+	protected IntInvert(
+			CFG cfg,
+			CodeLocation location,
+			String constructName,
+			Expression self) {
+		super(cfg, location, constructName, self);
+	}
+
+	public static IntInvert build(
+			CFG cfg,
+			CodeLocation location,
+			Expression[] exprs) {
+		return new IntInvert(cfg, location, "__invert__", exprs[0]);
+	}
+
+	@Override
+	final public void setOriginatingStatement(
+			Statement st) {
+		this.st = st;
+	}
+
+	@Override
+	protected int compareSameClassAndParams(
+			Statement o) {
+		return 0;
+	}
+
+	@Override
+	public <A extends AbstractLattice<A>, D extends AbstractDomain<A>> AnalysisState<A> fwdUnarySemantics(
+			InterproceduralAnalysis<A, D> interprocedural,
+			AnalysisState<A> state,
+			SymbolicExpression expr,
+			StatementStore<A> expressions)
+			throws SemanticException {
+		return interprocedural.getAnalysis().smallStepSemantics(state,
+				new it.unive.lisa.symbolic.value.UnaryExpression(
+						getStaticType(),
+						expr,
+						BitwiseNegation.INSTANCE,
+						getLocation()),
+				st);
+	}
+}
