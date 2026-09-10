9224b80856a56c08782d421945d64e6e61ed9529
VincenzoArceri <vincenzoarceri.92@gmail.com>
2026-09-10 11:05:46 +0200

    List are allocated in the heap

--- pylisa/src/main/java/it/unive/pylisa/cfg/expression/ListCreation.java
+++ pylisa/src/main/java/it/unive/pylisa/cfg/expression/ListCreation.java
@@ -2,6 +2,7 @@ package it.unive.pylisa.cfg.expression;
 
 import it.unive.lisa.analysis.AbstractDomain;
 import it.unive.lisa.analysis.AbstractLattice;
+import it.unive.lisa.analysis.Analysis;
 import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.StatementStore;
@@ -12,16 +13,18 @@ import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.statement.Expression;
 import it.unive.lisa.program.cfg.statement.NaryExpression;
 import it.unive.lisa.program.cfg.statement.Statement;
+import it.unive.lisa.program.type.Int32Type;
 import it.unive.lisa.symbolic.SymbolicExpression;
-import it.unive.lisa.symbolic.value.BinaryExpression;
-import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.heap.AccessChild;
+import it.unive.lisa.symbolic.heap.HeapDereference;
+import it.unive.lisa.symbolic.heap.HeapReference;
+import it.unive.lisa.symbolic.heap.MemoryAllocation;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.type.ReferenceType;
 import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
 import it.unive.pylisa.cfg.type.PyClassType;
 import it.unive.pylisa.libraries.LibrarySpecificationProvider;
-import it.unive.pylisa.symbolic.ListConstant;
-import it.unive.pylisa.symbolic.operators.ListAppend;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ListCreation extends NaryExpression {
 
@@ -45,32 +48,28 @@ public class ListCreation extends NaryExpression {
 			ExpressionSet[] params,
 			StatementStore<A> expressions)
 			throws SemanticException {
+		Analysis<A, D> analysis = interprocedural.getAnalysis();
 		CodeLocation loc = getLocation();
-		ListConstant list = new ListConstant(loc);
 
-		if (params.length == 0)
-			return interprocedural.getAnalysis().smallStepSemantics(state, list, this);
+		Type listType = PyClassType.lookup(LibrarySpecificationProvider.LIST);
+		ReferenceType reftype = new ReferenceType(listType);
+		MemoryAllocation created = new MemoryAllocation(listType, loc, false);
+		HeapReference ref = new HeapReference(reftype, created, loc);
+		HeapDereference deref = new HeapDereference(listType, ref, loc);
 
-		Type listtype = PyClassType.lookup(LibrarySpecificationProvider.LIST);
-		BinaryOperator append = ListAppend.INSTANCE;
+		AnalysisState<A> current = analysis.smallStepSemantics(state, ref, this);
 
-		Set<BinaryExpression> ws = new HashSet<>(), tmp = new HashSet<>();
-		for (SymbolicExpression firstelement : params[0])
-			ws.add(new BinaryExpression(listtype, list, firstelement, append, loc));
+		for (int i = 0; i < params.length; i++) {
+			AccessChild slot = new AccessChild(Untyped.INSTANCE, deref, new Constant(Int32Type.INSTANCE, i, loc), loc);
+			AnalysisState<A> slotState = analysis.smallStepSemantics(current, slot, this);
 
-		for (int i = 1; i < params.length; i++) {
-			tmp.addAll(ws);
-			ws.clear();
+			AnalysisState<A> next = state.bottom();
 			for (SymbolicExpression element : params[i])
-				for (BinaryExpression listhead : tmp)
-					ws.add(new BinaryExpression(listtype, listhead, element, append, loc));
-			tmp.clear();
+				for (SymbolicExpression slotId : slotState.getExecutionExpressions())
+					next = next.lub(analysis.assign(slotState, slotId, element, this));
+			current = next;
 		}
 
-		AnalysisState<A> result = state.bottom();
-		for (BinaryExpression completelist : ws)
-			result = result.lub(interprocedural.getAnalysis().smallStepSemantics(state, completelist, this));
-
-		return result;
+		return analysis.smallStepSemantics(current, ref, this);
 	}
 }
